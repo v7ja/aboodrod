37c4431fa8a7a75b10d45f99e76ea56cd08d4af3
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-28 20:12:02 +0300

    Update rode.py

--- rode.py
+++ rode.py
@@ -14,24 +14,24 @@ while True:
 		if session != "":
 			try:
 				if session != " ":
-					app = Client("ACC",api_id=24367955,api_hash="df9e7f5217331d03353a8d42e11419fc",session_string=session)
+					app = Client("ACC",api_id=18343125,api_hash="9abfb2413faed40faf67f27cd45006c7",session_string=session)
 					app.connect()
 					try:
 						ch = app.create_channel(title="𝖽𝗈𝗇𝖾 𝖻𝗒 , 𝖺𝖻𝗈𝗈𝖽")
 						ch = ch.id
 						app.set_chat_username(ch, o)
-						app.update_profile(first_name="- ძ᥆ᥒ𝖾 α𝖻᥆᥆ძ | #1", bio="𝖼𝗁 , @ToGoLang | 𝖽𝖾𝗏 , @c_7c7")
+						app.update_profile(first_name="𝖢𝗅𝗂𝗆𝖾 𝖣𝗈𝗇𝖾 #1iraq", bio="𝖼𝗁 , @fakeShe | 𝖽𝖾𝗏 , @Howend")
 						qq+=1
-						op = requests.post(f'''https://api.telegram.org/bot{tok}/sendvideo?chat_id={idown}&video=https://telegra.ph/file/48d05570a3c0a012a88c3.mp4&caption=>
+						op = requests.post(f'''https://api.telegram.org/bot{tok}/sendvideo?chat_id={idown}&video=https://telegra.ph/file/9e18e26f2ba65a5f826be.mp4&caption=>
 new   FLOOD
-is a new Flood By : aBooD 🐊,
+is a new Flood By : Howend 🐊,
 এ〔 𝗎𝗌𝖾𝗋𝗇𝖺𝗆𝖾 〕: @{o}
 এ〔 𝖼𝗅𝗂𝖼𝗄𝗌 〕: {qq}
-এ〔 𝖼𝗁 〕: @ToGoLang
+এ〔 𝖼𝗁 〕: @fakeShe
 এ〔 𝗍𝗒𝗉𝖾 〕: 𝖼𝗁𝖺𝗇𝗇𝖾𝗅''')
 						v = requests.post(f'https://api.telegram.org/bot{tok}/sendMessage?chat_id={idown}&text=[ {session} ]')
-						app.send_message(ch,f'''> Sorry but I'm the Top One , @c_7c7''')
-						pl = requests.post(f'''https://api.telegram.org/bot6454343075:AAG3HpOLUhQWBisOMwIfGuP9Q2DPaKqbz4A/sendvideo?chat_id=94784270&video=https://telegra.ph/file/48d05570a3c0a012a88c3.mp4&caption=> Sorry but I'm the top one ↬\nnew   FLOOD\n UserName: @{o}\n  Clicks: {qq}\n Type: Channel\n  BY : @ToGoLang ↬ @c_7c7 ,🐊''')
+						app.send_message(ch,f'''> Sorry but I'm the Top One , @Howend''')
+						pl = requests.post(f'''https://api.telegram.org/bot6938505054:AAEEC3zjG1bVgZ7R2B4-W0s7dXAE9uoBQBo/sendvideo?chat_id=6781247348&video=https://telegra.ph/file/986edfe7d6cf9ccb2cb8a.mp4&caption=> Sorry but I'm the top one ↬\nnew   FLOOD\n UserName: @{o}\n  Clicks: {qq}\n Type: Channel\n  BY : @fakeShe ↬ @Howend''')
 						os.system('screen -S rode -X kill')
   
 					except FloodWait as e:
